5dae8e9554aedc198c1bb06b19e9947782f8c468
NapatWejpreecha <napatwejpreecha@gmail.com>
2021-05-30 14:35:28 +0700

    Added code

---
+++ Exercise4_Napat_W.py
@@ -0,0 +1,9 @@
+FD_Eng = "Foundation English = 88"
+GN_Bus = "General Business = 75"
+Intro_Com = "Introduction to Computer Systems = 95"
+Com_Pro = "Computer Programmimg = 87"
+print("---Youe Score---")
+print(FD_Eng)
+print(GN_Bus)
+print(Intro_Com)
+print(Com_Pro)
