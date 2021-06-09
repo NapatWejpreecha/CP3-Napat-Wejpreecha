62466e469450c2c5c8fb0302493cb347a51ae291
NapatWejpreecha <napatwejpreecha@gmail.com>
2021-06-09 14:46:02 +0700

    Create Lecture50_Napat_w.py

---
+++ Lecture50_Napat_w.py
@@ -0,0 +1,12 @@
+def addNumber(x,y):
+    print(x+y)
+def deleteNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+addNumber(5,10)
+deleteNumber(5,10)
+multiplyNumber(5,10)
+divideNumber(5,10)
